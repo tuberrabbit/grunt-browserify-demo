a5b689b211a9383313579b40e3bcc2ccb8781c3c
tuber <btu@thoughtworks.com>
2016-03-25 17:29:47 +0800

    [tuber] update drop function

--- app/app.js
+++ app/app.js
@@ -9,5 +9,8 @@ var allowDrop = function (ev) {
 var drop = function (ev) {
     ev.preventDefault();
     var ele = document.getElementById(ev.dataTransfer.getData('img'));
-    ele.className += ' animation--fade-out';
+    ele.classList.add('animation--fade-out');
+    setTimeout(function () {
+        ele.classList.remove('animation--fade-out');
+    }, 2001);
 };
