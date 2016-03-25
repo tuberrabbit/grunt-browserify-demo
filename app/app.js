06a4a7cdb847bae8e587eb708ed9c80c37778988
tuber <btu@thoughtworks.com>
2016-03-25 17:26:47 +0800

    [tuber] try drap & drop event

---
+++ app/app.js
@@ -0,0 +1,13 @@
+var drag = function (ev) {
+    ev.dataTransfer.setData('img', ev.target.id);
+};
+
+var allowDrop = function (ev) {
+    ev.preventDefault();
+};
+
+var drop = function (ev) {
+    ev.preventDefault();
+    var ele = document.getElementById(ev.dataTransfer.getData('img'));
+    ele.className += ' animation--fade-out';
+};
