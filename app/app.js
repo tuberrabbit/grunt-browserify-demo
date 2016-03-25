5234720f7e20367fbe1b1dbf23bae223e9b9a782
tuber <btu@thoughtworks.com>
2016-03-25 20:16:25 +0800

    [tuber] finish css performance

--- app/app.js
+++ app/app.js
@@ -1,16 +1,43 @@
-var drag = function (ev) {
-    ev.dataTransfer.setData('img', ev.target.id);
-};
+const IMAGE_SRC = [
+    './images/img1.jpeg',
+    './images/img2.jpeg'
+];
+
+var img1Src = 0;
+var img2Src = 1;
+
+var img1 = document.getElementById('img1');
+img1.src = IMAGE_SRC[img1Src];
+var img2 = document.getElementById('img2');
+img2.src = IMAGE_SRC[img2Src];
 
 var allowDrop = function (ev) {
     ev.preventDefault();
 };
 
+var getAnimationClassByDirection = function (direction) {
+    switch (direction.split('-')[2]) {
+        case 'top':
+            return 'fadeOutUpBig';
+        case 'right':
+            return 'fadeOutRightBig';
+        case 'bottom':
+            return 'fadeOutDownBig';
+        case 'left':
+            return 'fadeOutLeftBig';
+    }
+};
+
 var drop = function (ev) {
     ev.preventDefault();
-    var ele = document.getElementById(ev.dataTransfer.getData('img'));
-    ele.classList.add('animation--fade-out');
+    var animationClass = getAnimationClassByDirection(ev.target.id);
+    img1.classList.add('animated', animationClass);
     setTimeout(function () {
-        ele.classList.remove('animation--fade-out');
-    }, 2001);
+        img1Src ^= img2Src;
+        img2Src ^= img1Src;
+        img1Src ^= img2Src;
+        img1.src = IMAGE_SRC[img1Src];
+        img2.src = IMAGE_SRC[img2Src];
+        img1.classList.remove('animated', animationClass);
+    }, 500);
 };
